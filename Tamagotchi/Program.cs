4f48d3a72a3d08d71878b3230482489c2414590a
August <august.agren@elev.ga.ntig.se>
2020-10-07 11:11:10 +0200

    tamagotchi skapelse

--- Tamagotchi/Program.cs
+++ Tamagotchi/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Threading;
+using System;
 
 namespace Tamagotchi
 {
@@ -6,7 +7,47 @@ namespace Tamagotchi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Tamagotchi tamagotchi = new Tamagotchi();
+
+            Console.WriteLine("What will you name your tamagotchi?");
+            
+            tamagotchi.name = Console.ReadLine();
+
+            Console.WriteLine(tamagotchi.name + ", what a lovley name!");
+            
+            sleep();
+
+            while(tamagotchi.GetAlive() == true)
+            {
+                tamagotchi.PrintStats();
+
+                sleep();
+
+                Console.WriteLine("please choosewhat you want to do with " + tamagotchi.name +
+                "\n[Teach]" +
+                "\n[Feed]" +
+                "\n[Speak]");
+                // + "\n[Kill it!]"
+                // + "\n[add another]"
+
+                string answer = Console.ReadLine();
+
+                if(answer.ToLower() == "teach")
+                {
+                    Console.WriteLine("Word to teach");
+                    tamagotchi.word = Console.ReadLine();
+                    
+                    tamagotchi.Teach();
+                }
+
+            }
+
+            Console.ReadLine();
+        }
+
+        public static void sleep()
+        {
+            Thread.Sleep(2000);
         }
     }
 }
--- Tamagotchi/Tamagotchi.cs
+++ Tamagotchi/Tamagotchi.cs
@@ -5,31 +5,32 @@ namespace Tamagotchi
 {
     public class Tamagotchi
     {
-        private Random generator = new Random();
+        private static Random generator = new Random();
 
-        private int hunger = 0;
+        private int hunger = 10;
 
-        private int boredom = 0;
+        private int boredom = 10;
 
         private List<string> words = new List<string>();
 
         private bool isAlive = true;
 
-        public string name = Console.ReadLine();
+        public string name = ("");
         
         public void Feed()
         {
             if (hunger >= 0)
             {
-                hunger -=10;
+                hunger +=1;
                 Console.WriteLine("You fed your Tamagotchi");
             }
 
             else
             {
-                Console.WriteLine("Your Tamagotchi is full");
+                Console.WriteLine(name + " is full");
             }
         }
+
         public void Hi()
         {
             if (words.Count >= 1)
@@ -39,43 +40,65 @@ namespace Tamagotchi
                 string sayWord = words[whatWord];
 
                 Console.WriteLine(sayWord);
-
+                
                 ReduceBoredom();
             }
             else
             {
-                Console.WriteLine("Your Tamagotchi don't know any words");
+                Console.WriteLine(name + " don't know any words");
                 Console.WriteLine("You need to teach your Tamagotchi words");
             }
 
         }
+
         public void Teach(string word)
         {
             words.Add(word);
+            ReduceBoredom();
         }
+
         public void Tick()
         {
+            hunger -= 1;
+            boredom -= 1;
 
+            if(hunger <= 0 || boredom <= 0)
+            {
+                isAlive = false;
+            }
         }
+
         public void PrintStats()
         {
+            Console.WriteLine(name + " har:");
+            Console.WriteLine(hunger + "/10 Hunger");
+            Console.WriteLine(boredom + "/10 Boredom");
 
+            if(isAlive == true)
+            {
+                Console.WriteLine("Your abomenation is alive");
+            }
+            else
+            {
+                Console.WriteLine("Your creature is dead");
+            }
         }
-        public void GetAlive()
+
+        public bool GetAlive()
         {
-
+            return isAlive;
         }
+
         private void ReduceBoredom()
         {
             if (boredom >= 0)
             {
-                boredom -= 10;
-                Console.WriteLine("Your Tamagotchi is not as bored.");
+                boredom += 1;
+                Console.WriteLine(name + " is not as bored.");
             }
-
             else
             {
-                Console.WriteLine("Your Tamagotchi is not bored at all.");
+                Console.WriteLine(name + " is not bored at all.");
             }
             
         }
