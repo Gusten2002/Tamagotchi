3ea3d469e839092dac3a68dcafa53a7a700a2f16
August <august.agren@elev.ga.ntig.se>
2020-10-13 11:06:48 +0200

    återskapa en tamagotchi efter att den har dött

--- Tamagotchi/Program.cs
+++ Tamagotchi/Program.cs
@@ -7,69 +7,84 @@ namespace Tamagotchi
     {
         static void Main(string[] args)
         {
-            Tamagotchi tamagotchi = new Tamagotchi();
+            string playMore = "yes";
 
-            Console.WriteLine("What will you name your tamagotchi?");
-            
-            tamagotchi.name = Console.ReadLine();
-
-            Console.WriteLine(tamagotchi.name + ", what a lovley name!");
-            
-            sleep();
-
-            while(tamagotchi.GetAlive() == true)
+            while (playMore == "yes")
             {
-                tamagotchi.PrintStats();
+                Tamagotchi tamagotchi = new Tamagotchi();
 
-                sleep();
+                bool carryOn = true;
 
-                Console.WriteLine("please choosewhat you want to do with " + tamagotchi.name +
-                "\n[Teach]" +
-                "\n[Feed]" +
-                "\n[Speak]" +
-                "\n[Nothing]" );
-                // + "\n[Kill it!]"
-                // + "\n[add another]");
-
-                string answer = Console.ReadLine();
-
-                while(answer.ToLower() != "teach" && answer.ToLower() != "feed" && answer.ToLower() != "speak" && answer.ToLower() != "nothing")
+                while (carryOn == true)
                 {
-                    Console.WriteLine("Only accepting any of the four choices");
-                    answer = Console.ReadLine();
-                }
+                    Console.WriteLine("What will you name your tamagotchi?");
 
-                if(answer.ToLower() == "teach")
-                {
-                    Console.WriteLine("Word to teach");
-                    string learnWord = Console.ReadLine();
-                    
-                    tamagotchi.Teach(learnWord);
-                }
+                    tamagotchi.name = Console.ReadLine();
 
-                else if(answer.ToLower() == "feed")
-                {
-                    Console.WriteLine("You feed your tamagotchi");
-                    tamagotchi.Feed();
-                }
-
-                else if(answer.ToLower() == "speak")
-                {
-                    tamagotchi.Hi();
-                }
-
-                if(answer.ToLower() == "nothing")
-                {
-                    Console.WriteLine("You did nothing.");
-                }
-
-                    tamagotchi.Tick();
-                    
+                    Console.WriteLine(tamagotchi.name + ", what a lovley name!");
                     sleep();
-                    Console.Clear();
+
+                    while (tamagotchi.GetAlive() == true)
+                    {
+                        tamagotchi.PrintStats();
+                        sleep();
+
+                        Console.WriteLine("please choosewhat you want to do with " + tamagotchi.name +
+                        "\n[Teach]" +
+                        "\n[Feed]" +
+                        "\n[Speak]" +
+                        "\n[Nothing]");
+                        // + "\n[Kill it!]"
+                        // + "\n[add another]");
+
+                        string answer = Console.ReadLine().ToLower();
+
+                        while (answer != "teach" && answer != "feed" && answer != "speak" && answer != "nothing")
+                        {
+                            Console.WriteLine("Only accepting any of the four choices");
+                            answer = Console.ReadLine();
+                        }
+
+                        if (answer.ToLower() == "teach")
+                        {
+                            Console.WriteLine("Word to teach");
+                            string learnWord = Console.ReadLine();
+
+                            tamagotchi.Teach(learnWord);
+                        }
+
+                        else if (answer.ToLower() == "feed")
+                        {
+                            Console.WriteLine("You feed your tamagotchi");
+                            tamagotchi.Feed();
+                        }
+
+                        else if (answer.ToLower() == "speak")
+                        {
+                            tamagotchi.Hi();
+                        }
+
+                        if (answer.ToLower() == "nothing")
+                        {
+                            Console.WriteLine("You did nothing.");
+                        }
+
+                        tamagotchi.Tick();
+
+                        sleep();
+                        Console.Clear();
+                    }
+
+                    Console.WriteLine("Do you want to create a new tamagotchi?" +
+                    "\n[Yes]" +
+                    "\n[No]");
+
+                    playMore = Console.ReadLine().ToLower();
+                }
+                
+                Console.ReadLine();
             }
 
-            Console.ReadLine();
         }
 
         public static void sleep()
